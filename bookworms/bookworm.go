aae2bc5cfcff22b00a39e65617479259a6058a48
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2026-07-29 15:06:28 -0500

    added loadBookworms function

--- bookworms/bookworm.go
+++ bookworms/bookworm.go
@@ -1,1 +1,7 @@
-// add business logic
+package main
+
+// loadBookworms reads the file and returns the list of
+// bookworms and their books
+func loadBookworms(filePath string) ([] Bookworm, error) {
+	return nil, nil
+}
