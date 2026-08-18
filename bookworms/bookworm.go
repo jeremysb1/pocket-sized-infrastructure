cdd8a84702096fa4952d4dc0f2bd821eeeb77cef
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2026-08-18 15:01:31 -0500

    imported packages and Bookworm struct

--- bookworms/bookworm.go
+++ bookworms/bookworm.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+)
+
+// A Bookworm contains a list of books on a bookworm's shelf.
+type Bookworm struct {
+	Name string `json:"name"`
+	Books []Book `json:"books"`
+}
+
 // loadBookworms reads the file and returns the list of
 // bookworms and their books
 func loadBookworms(filePath string) ([]Bookworm, error) {
