5f4ad8accf1ad706a341e77df7dabb7e78112cb4
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2026-08-22 12:56:37 -0500

    added book test variables

--- bookworms/bookworms_internal_test.go
+++ bookworms/bookworms_internal_test.go
@@ -5,3 +5,9 @@ import (
 	"testing"
 )
 
+var (
+	handMaidsTale = Book {Author: "Margaret Atwood", Title: "The Handmaid's Tale",}
+	oryxAndCrake = Book {Author: "Margaret Atwood", Title: "Oryx and Crake"}
+	theBellJar = Book {Author: "Sylvia Plath", Title: "The Bell Jar"}
+	janeEyre = Book {Author: "Charlotte Brontë", Title: "Jane Eyre"}
+)
